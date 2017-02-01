c64ae0f43f5f74af31e7e93af8f4e615110b1d65
Ar11rA <aritra.ghosh2295@yahoo.com>
2017-02-01 12:01:30 +0530

    Added the functionality for calculating the current coordinate of the bot

---
+++ botCoordinateCalculation.js
@@ -0,0 +1,21 @@
+function movementCoordinates(element, bot, gridsizeX, gridsizeY) {
+
+  if (bot.startY >= 0 && bot.startY < gridsizeY) {
+    if (element == 'M' && bot.dirBot == 'N') {
+      bot.startY--;
+      if (bot.startY == -1) bot.startY = 0;
+    }
+    else if (element == 'M' && bot.dirBot == 'S')
+      bot.startY++;
+  }
+  if (bot.startX >= 0 && bot.startX < gridsizeX) {
+    if (element == 'M' && bot.dirBot == 'W') {
+      bot.startX--;
+      if (bot.startX == -1) bot.startX = 0;
+    }
+    else if (element == 'M' && bot.dirBot == 'E')
+      bot.startX++;
+  }
+  return bot;
+}
+module.exports = movementCoordinates;
