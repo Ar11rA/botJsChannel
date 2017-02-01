a0b245fb1dc1af7493161fe9461e40b4236644d3
Ar11rA <aritra.ghosh2295@yahoo.com>
2017-02-01 12:41:03 +0530

    Modified the functionalities, coordinate calculation and setting direction

--- setBotDirection.js
+++ setBotDirection.js
@@ -1,19 +1,19 @@
 function setBotDirection(element, bot, gridSizeX, gridSizeY) {
-  if (element == 'L' && bot.dirBot == 'N')
+  if (element === 'L' && bot.dirBot === 'N')
     bot.dirBot = 'W';
-  else if (element == 'R' && bot.dirBot == 'N')
+  else if (element === 'R' && bot.dirBot === 'N')
     bot.dirBot = 'E';
-  else if (element == 'L' && bot.dirBot == 'S')
+  else if (element === 'L' && bot.dirBot === 'S')
     bot.dirBot = 'E';
-  else if (element == 'R' && bot.dirBot == 'S')
+  else if (element === 'R' && bot.dirBot === 'S')
     bot.dirBot = 'W';
-  else if (element == 'L' && bot.dirBot == 'W')
+  else if (element === 'L' && bot.dirBot === 'W')
     bot.dirBot = 'S';
-  else if (element == 'R' && bot.dirBot == 'W')
+  else if (element === 'R' && bot.dirBot === 'W')
     bot.dirBot = 'N';
-  else if (element == 'L' && bot.dirBot == 'E')
+  else if (element === 'L' && bot.dirBot === 'E')
     bot.dirBot = 'N';
-  else if (element == 'R' && bot.dirBot == 'E')
+  else if (element === 'R' && bot.dirBot === 'E')
     bot.dirBot = 'S';
   return bot;
 }
