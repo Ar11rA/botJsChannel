11df8fd7cb6dce0b730d8e4a87c4b6e6e5260c8a
Ar11rA <aritra.ghosh2295@yahoo.com>
2017-02-01 12:02:27 +0530

    Added the functionality to calculate bot direction according to given instruction

---
+++ setBotDirection.js
@@ -0,0 +1,20 @@
+function setBotDirection(element, bot, gridSizeX, gridSizeY) {
+  if (element == 'L' && bot.dirBot == 'N')
+    bot.dirBot = 'W';
+  else if (element == 'R' && bot.dirBot == 'N')
+    bot.dirBot = 'E';
+  else if (element == 'L' && bot.dirBot == 'S')
+    bot.dirBot = 'E';
+  else if (element == 'R' && bot.dirBot == 'S')
+    bot.dirBot = 'W';
+  else if (element == 'L' && bot.dirBot == 'W')
+    bot.dirBot = 'S';
+  else if (element == 'R' && bot.dirBot == 'W')
+    bot.dirBot = 'N';
+  else if (element == 'L' && bot.dirBot == 'E')
+    bot.dirBot = 'N';
+  else if (element == 'R' && bot.dirBot == 'E')
+    bot.dirBot = 'S';
+  return bot;
+}
+module.exports = setBotDirection;
